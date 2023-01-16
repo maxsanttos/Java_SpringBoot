696fc084c03dacb9709c6383b631a8e46d39cdb1
Max Suel <maxsanttos@hotmail>
2023-01-16 14:42:26 -0300

    update

---
+++ curso-spring-boot-especialista-02_02_scripts_criacao_base_dados/src/main/java/io/github/maxsanttos/domain/entity/Pedido.java
@@ -0,0 +1,44 @@
+package io.github.maxsanttos.domain.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Pedido {
+
+    private Integer id;
+    private Cliente cliente;
+    private LocalDate dataPedido;
+    private BigDecimal valorTotal;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDate getDataPedido() {
+        return dataPedido;
+    }
+
+    public void setDataPedido(LocalDate dataPedido) {
+        this.dataPedido = dataPedido;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+}
