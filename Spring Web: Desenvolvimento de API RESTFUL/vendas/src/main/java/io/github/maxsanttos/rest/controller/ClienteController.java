be3bb5cd58963b42d92720af31ad7b60a85d0565
Max Suel <maxsanttos@hotmail>
2023-01-22 13:27:25 -0300

    controler

---
+++ Spring Web: Desenvolvimento de API RESTFUL/vendas/src/main/java/io/github/maxsanttos/rest/controller/ClienteController.java
@@ -0,0 +1,17 @@
+package io.github.maxsanttos.rest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api/clientes")
+public class ClienteController {
+
+    @RequestMapping(value = "/hello/{nome}")
+    @ResponseBody
+    public String helloCliente(@PathVariable("nome") String nomeCliente){
+        return String.format("Hello %s" + nomeCliente);
+    }
+}
