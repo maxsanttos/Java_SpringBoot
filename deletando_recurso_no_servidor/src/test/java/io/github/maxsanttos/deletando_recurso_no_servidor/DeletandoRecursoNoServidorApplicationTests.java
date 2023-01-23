dbf0b70c911caadd7676dcb164d43a86fbdc6080
Max Suel <maxsanttos@hotmail>
2023-01-23 14:30:53 -0300

    Delete Mapping: deletando um recurso no servido

---
+++ deletando_recurso_no_servidor/src/test/java/io/github/maxsanttos/deletando_recurso_no_servidor/DeletandoRecursoNoServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.maxsanttos.deletando_recurso_no_servidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeletandoRecursoNoServidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
