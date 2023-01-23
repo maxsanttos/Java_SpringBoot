dd98b1f9472cc9cddd2588236acde4ba97a079e1
Max Suel <maxsanttos@hotmail>
2023-01-23 14:53:56 -0300

    criando class controller

---
+++ deletando_recurso_no_servidor/src/main/java/io/github/maxsanttos/deletando_recurso_no_servidor/rest/controller/ClienteController.java
@@ -0,0 +1,35 @@
+package io.github.maxsanttos.deletando_recurso_no_servidor.rest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
+
+import io.github.maxsanttos.deletando_recurso_no_servidor.domain.entity.Cliente;
+import io.github.maxsanttos.deletando_recurso_no_servidor.domain.repository.Clientes;
+
+@Controller
+public class ClienteController {
+    
+    private Clientes clientes;
+
+    @GetMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity getClienteById( @PathVariable Integer id ){
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if(cliente.isPresent()){
+            return ResponseEntity.ok( cliente.get() );
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/api/clientes")
+    @ResponseBody
+    public  ResponseEntity save(@RequestBody Cliente cliente){
+        Cliente clienteSalvo = clientes.save(cliente);
+
+        return ResponseEntity.ok(clienteSalvo);
+    }
+}
