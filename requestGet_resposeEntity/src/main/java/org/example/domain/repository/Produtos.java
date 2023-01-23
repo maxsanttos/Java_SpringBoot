3007bf22814f9382c7b9340714b3f93ca2fbac71
Max Suel <maxsanttos@hotmail>
2023-01-23 14:18:11 -0300

    request mapping, requisiçao post e request body

--- requestGet_resposeEntity/src/main/java/org/example/domain/repository/Produtos.java
+++
@@ -1,7 +0,0 @@
-package org.example.domain.repository;
-
-import org.example.domain.entity.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Produtos extends JpaRepository<Produto,Integer> {
-}
--- requestGet_resposeEntity/src/main/java/org/example/domain/repository/itemsPedido.java
+++
@@ -1,7 +0,0 @@
-package org.example.domain.repository;
-
-import org.example.domain.entity.ItemPedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface itemsPedido extends JpaRepository<ItemPedido,Integer> {
-}
