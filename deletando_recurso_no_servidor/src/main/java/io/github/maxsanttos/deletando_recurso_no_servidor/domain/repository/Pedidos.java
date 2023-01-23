c2453b96ef33d99ceb833a0e8f78bbfd5e0c4aa9
Max Suel <maxsanttos@hotmail>
2023-01-23 14:49:45 -0300

    Delete Mapping: criando repositorios

---
+++ deletando_recurso_no_servidor/src/main/java/io/github/maxsanttos/deletando_recurso_no_servidor/domain/repository/Pedidos.java
@@ -0,0 +1,12 @@
+package io.github.maxsanttos.deletando_recurso_no_servidor.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.maxsanttos.deletando_recurso_no_servidor.domain.entity.Cliente;
+import io.github.maxsanttos.deletando_recurso_no_servidor.domain.entity.Pedido;
+
+public interface Pedidos extends JpaRepository<Pedido, Integer>{
+    List<Pedido> findByCliente(Cliente cliente);
+}
---
+++ deletando_recurso_no_servidor/src/main/java/io/github/maxsanttos/deletando_recurso_no_servidor/domain/repository/Produtos.java
@@ -0,0 +1,9 @@
+package io.github.maxsanttos.deletando_recurso_no_servidor.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.maxsanttos.deletando_recurso_no_servidor.domain.entity.Produto;
+
+public interface Produtos extends JpaRepository<Produto,Integer> {
+    
+}
