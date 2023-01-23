17684f47e5860da11d708091453c1acf1c55c916
Max Suel <maxsanttos@hotmail>
2023-01-23 14:20:22 -0300

    Requisicao POST e Request Body

---
+++ Requisicao_GET_com_paramentros_Response_Entity/src/main/java/io/github/dougllasfps/rest/controller/ClienteController.java
@@ -0,0 +1,41 @@
+package io.github.dougllasfps.rest.controller;
+
+import io.github.dougllasfps.domain.entity.Cliente;
+import io.github.dougllasfps.domain.repository.Clientes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+public class ClienteController {
+
+    private Clientes clientes;
+
+    public ClienteController( Clientes clientes ) {
+        this.clientes = clientes;
+    }
+
+    @GetMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity getClienteById( @PathVariable Integer id ){
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if(cliente.isPresent()){
+            return ResponseEntity.ok( cliente.get() );
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/api/clientes")
+    @ResponseBody
+    public  ResponseEntity save(@RequestBody Cliente cliente){
+        Cliente clienteSalvo = clientes.save(cliente);
+
+        return ResponseEntity.ok(clienteSalvo);
+    }
+
+
+}
