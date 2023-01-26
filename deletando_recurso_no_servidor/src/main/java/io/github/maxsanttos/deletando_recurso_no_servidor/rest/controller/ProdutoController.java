a7c9782a751995aef845fecfcfe3ed8c00f044d9
Max Suel <maxsanttos@hotmail>
2023-01-26 14:01:42 -0300

    updates

--- deletando_recurso_no_servidor/src/main/java/io/github/maxsanttos/deletando_recurso_no_servidor/rest/controller/ProdutoController.java
+++ deletando_recurso_no_servidor/src/main/java/io/github/maxsanttos/deletando_recurso_no_servidor/rest/controller/ProdutoController.java
@@ -1,9 +1,7 @@
 package io.github.maxsanttos.deletando_recurso_no_servidor.rest.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.maxsanttos.deletando_recurso_no_servidor.domain.repository.Produtos;
@@ -19,24 +17,5 @@ public class ProdutoController {
         this.produtos = produtos;
     }
 
-    @PostMapping
-    @ResponseStatus()
-    public Produto save( @RequestBody Produto produto ){
-        return repository.save(produto);
-    }
-
-    @PutMapping("{id}")
-    @ResponseStatus(NO_CONTENT)
-    public void update( @PathVariable Integer id, @RequestBody Produto produto ){
-        repository
-                .findById(id)
-                .map( p -> {
-                   produto.setId(p.getId());
-                   repository.save(produto);
-                   return produto;
-                }).orElseThrow( () ->
-                new ResponseStatusException(HttpStatus.NOT_FOUND,
-                        "Produto não encontrado."));
-    }
     
 }
