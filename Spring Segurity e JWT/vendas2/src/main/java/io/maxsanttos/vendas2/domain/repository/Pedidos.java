97dc2f25bcf9ad3022eed071ca5642147f8be592
Max Santtos <64488195+maxsanttos@users.noreply.github.com>
2023-02-22 16:05:41 -0300

    Update Criando a interface Pedido finalizado

---
+++ Spring Segurity e JWT/vendas2/src/main/java/io/maxsanttos/vendas2/domain/repository/Pedidos.java
@@ -0,0 +1,15 @@
+package io.maxsanttos.vendas2.domain.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
+import java.util.Optional;
+import io.maxsanttos.vendas2.domain.entity.Pedido;
+import io.maxsanttos.vendas2.domain.entity.Cliente;
+
+public interface Pedidos extends JpaRepository<Pedido,Integer>{
+    List<Pedido> findByCliente(Cliente cliente);
+
+    @Query(" select p from Pedido p left join fetch p.itens where p.id = :id ")
+    Optional<Pedido> findByIdFetchItens(@Param("id") Integer id);
+}
