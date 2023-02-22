ee4eaafee1e6b1ee8f5f06bbd255026a825324cb
Max Santtos <64488195+maxsanttos@users.noreply.github.com>
2023-02-22 16:02:23 -0300

    Update Criando a interface ItemsPedido finalizado

---
+++ Spring Segurity e JWT/vendas2/src/main/java/io/maxsanttos/vendas2/domain/repository/ItemsPedido.java
@@ -0,0 +1,7 @@
+package io.maxsanttos.vendas2.domain.repository;
+import io.maxsanttos.vendas2.domain.entity.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemsPedido extends JpaRepository<ItemPedido, Integer> {
+    
+}
