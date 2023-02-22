d903132e662d5e12ecc5407898c2fe85b1c044e0
Max Santtos <64488195+maxsanttos@users.noreply.github.com>
2023-02-22 15:23:44 -0300

    Pedido
    Concluindo arquivos de Pedido

--- Spring Segurity e JWT/vendas2/src/main/java/io/maxsanttos/vendas2/domain/entity/Pedido.java
+++ Spring Segurity e JWT/vendas2/src/main/java/io/maxsanttos/vendas2/domain/entity/Pedido.java
@@ -1,5 +1,36 @@
 package io.maxsanttos.vendas2.domain.entity;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "pedido")
 public class Pedido {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @Column(name = "data_pedido")
+    private LocalDate dataPedido;
+
+    @Column(name = "total", precision = 20, scale = 2)
+    private BigDecimal total;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private StatusPedido status;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens;
 }
