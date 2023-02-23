3ab22f7545d78635e5f42d5c24f99a09b62b6cc8
Max Santtos <64488195+maxsanttos@users.noreply.github.com>
2023-02-23 14:31:51 -0300

    Correções de bugs e melhorias

---
+++ Spring Segurity e JWT/vendas2/src/main/java/io/maxsanttos/vendas2/domain/rest/controller/ProdutoController.java
@@ -0,0 +1,71 @@
+package io.maxsanttos.vendas2.domain.rest.controller;
+
+import io.maxsanttos.vendas2.domain.repository.Produtos;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import static org.springframework.http.HttpStatus.*;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import java.util.List;
+import io.maxsanttos.vendas2.domain.entity.Produto;
+
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+    private Produtos repository;
+
+    public ProdutoController(Produtos repository){
+        this.repository = repository;
+    }
+
+    @PostMapping("{id}")
+    @ResponseStatus(NO_CONTENT)
+    public void update( @PathVariable Integer id, @RequestBody @Valid Produto produto ){
+        repository
+                .findById(id)
+                .map( p -> {
+                    produto.setId(p.getId());
+                    repository.save(produto);
+                    return produto;
+                }).orElseThrow( () ->
+                        new ResponseStatusException(NOT_FOUND,
+                                "Produto não encontrado."));
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(NO_CONTENT)
+    public void delete(@PathVariable Integer id){
+        repository
+                .findById(id)
+                .map( p -> {
+                    repository.delete(p);
+                    return Void.TYPE;
+                }).orElseThrow( () ->
+                        new ResponseStatusException(NOT_FOUND,
+                                "Produto não encontrado."));
+    }
+
+    @GetMapping("{id}")
+    public Produto getById(@PathVariable Integer id){
+        return repository
+                .findById(id)
+                .orElseThrow( () ->
+                        new ResponseStatusException(NOT_FOUND,
+                                "Produto não encontrado."));
+    }
+
+    @GetMapping
+    public List<Produto> find(Produto filtro ){
+        ExampleMatcher matcher = ExampleMatcher
+                .matching()
+                .withIgnoreCase()
+                .withStringMatcher(
+                        ExampleMatcher.StringMatcher.CONTAINING );
+
+        Example example = Example.of(filtro, matcher);
+        return repository.findAll(example);
+    }
+
+}
