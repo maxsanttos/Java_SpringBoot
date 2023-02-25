70401cdb9a36ad7f75d8cc3555461a7e07717f53
Max Santtos <64488195+maxsanttos@users.noreply.github.com>
2023-02-25 15:05:34 -0300

    adcionando os methods de security

--- Spring Segurity e JWT/vendas2/src/main/java/io/maxsanttos/vendas2/config/SecurityConfig.java
+++ Spring Segurity e JWT/vendas2/src/main/java/io/maxsanttos/vendas2/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package io.maxsanttos.vendas2.config;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -7,5 +9,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
     
+    //autenticação dos usuarios
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
 
+    //autorização
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
 }
