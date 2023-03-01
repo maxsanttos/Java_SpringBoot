fc1e37112375beedd56c386d1de897cfc4e3704f
Max Santtos <64488195+maxsanttos@users.noreply.github.com>
2023-03-01 13:14:13 -0300

    starting password encoder

--- Spring Segurity e JWT/vendas2/src/main/java/io/maxsanttos/vendas2/config/SecurityConfig.java
+++ Spring Segurity e JWT/vendas2/src/main/java/io/maxsanttos/vendas2/config/SecurityConfig.java
@@ -1,14 +1,24 @@
 package io.maxsanttos.vendas2.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
     
+    // criptografia e descriptografia
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
     //autenticação dos usuarios
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -21,3 +31,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         super.configure(http);
     }
 }
+ 
